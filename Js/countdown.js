7a8ba57f4bc81de696c0067b4199c04880360195
madina <madinamj@code.edu.az>
2023-08-14 08:16:04 +0400

    countdown&js added

---
+++ Js/countdown.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+    const targetTime = new Date().getTime() + 60 * 60 * 60 * 1000; // Current time + 60 hours
+  
+    setInterval(() => {
+      const currentTime = new Date().getTime();
+      const timeLeft = targetTime - currentTime;
+  
+      const hours = Math.floor(timeLeft / (1000 * 60 * 60));
+      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  
+      document.getElementById("hours").innerText = hours.toString().padStart(2, "0");
+      document.getElementById("minutes").innerText = minutes.toString().padStart(2, "0");
+      document.getElementById("seconds").innerText = seconds.toString().padStart(2, "0");
+    }, 1000);
+  }
+  
+  updateCountdown();
